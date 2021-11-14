ff9dc1b205f1c7009887cec4d43a56d2808cf6e9
Bishoy <bishoy.samwuel@gmail.com>
2021-11-14 17:55:27 +0200

    Fixing linter initial errors

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'web-vitals';
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    ['web-vitals'].then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
