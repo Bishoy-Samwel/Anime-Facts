6d04e27b82d87c07c663bcf1c348967650686227
Bishoy <bishoy.samwuel@gmail.com>
2021-11-18 16:33:17 +0200

    Apply basic navigation

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,14 @@
+import {
+  Switch,
+  Route,
+  // useParams,
+} from 'react-router-dom';
 import Header from './Header';
-import Banner from './Home/Banner';
 import styles from './Container.module.scss';
 import PageInfo from './PageInfo';
+import Banner from './ElementsInfo/Banner';
 import Elements from './ElementsInfo/Elements';
-// import { Details } from './ElementDetails/Details';
+import { Details } from './ElementDetails/Details';
 
 function Container() {
   return (
@@ -11,8 +16,14 @@ function Container() {
       <Header />
       <PageInfo />
       <Banner />
-      <Elements />
-      {/* <Details /> */}
+      <Switch>
+        <Route exact path="/">
+          <Elements />
+        </Route>
+        <Route path="/details">
+          <Details />
+        </Route>
+      </Switch>
     </div>
   );
 }
