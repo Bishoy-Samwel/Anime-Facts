3c70a510f6f501cea49070fb074a0fef464651c4
Bishoy <bishoy.samwuel@gmail.com>
2021-11-18 12:08:48 +0200

    Add PageInfo component

---
+++ src/components/PageInfo.js
@@ -0,0 +1,11 @@
+import styles from './PageInfo.module.scss';
+
+function PageInfo() {
+  return (
+    <div className={styles['page-info']}>
+      <p>Page Info</p>
+    </div>
+  );
+}
+
+export default PageInfo;
