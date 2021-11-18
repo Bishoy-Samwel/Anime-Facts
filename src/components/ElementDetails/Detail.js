e617b111ad3f5bebd4fef90c4dbca54c1bce2ac3
Bishoy <bishoy.samwuel@gmail.com>
2021-11-18 23:06:10 +0200

    Use UseParams to get the value of selected item

--- src/components/ElementDetails/Detail.js
+++ src/components/ElementDetails/Detail.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 import styles from './Detail.module.scss';
 
 const Detail = props => {
   const { detail } = props;
+  const { itemId } = useParams();
+
   return (
     <div className={styles['detail-div']} key={detail.id}>
       <div className={styles['right-div']}>
         {detail.name}
+        +
+        {itemId}
+
       </div>
       <div className={styles['left-div']}>
         <span>
