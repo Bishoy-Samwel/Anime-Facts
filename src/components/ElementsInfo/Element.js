53dd6a102be996f69e0014afb42eb90f2e81877e
Bishoy <bishoy.samwuel@gmail.com>
2021-11-18 23:07:24 +0200

    Passing a param to the url

--- src/components/ElementsInfo/Element.js
+++ src/components/ElementsInfo/Element.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BsArrowRightCircle } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
 import styles from './Element.module.scss';
 
 const Element = props => {
   const { element } = props;
   return (
     <div className={styles['element-div']} key={element.id}>
-      <BsArrowRightCircle className={styles['nav-arrow']} />
+      <NavLink to={`/details/${element.id}`} exact>
+        <BsArrowRightCircle color="white" className={styles['nav-arrow']} />
+      </NavLink>
       <div className={styles.top}>
         <img src="http://pngimg.com/uploads/chess/chess_PNG8443.png" alt="chess-family" />
       </div>
