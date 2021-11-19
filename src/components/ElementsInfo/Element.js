65a9542121e43bb831ff52913cc6d425062f427d
Bishoy <bishoy.samwuel@gmail.com>
2021-11-19 03:46:42 +0200

    Modify banner when you select the Item

--- src/components/ElementsInfo/Element.js
+++ src/components/ElementsInfo/Element.js
@@ -1,15 +1,22 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 import { BsArrowRightCircle } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 import styles from './Element.module.scss';
+import { modifyBanner } from '../../redux/elements';
 
 const Element = props => {
+  const dispatch = useDispatch();
   const { element } = props;
+  const clickHanlder = () => {
+    dispatch(modifyBanner(element.id));
+  };
   return (
     <div className={styles['element-div']} key={element.id}>
       <NavLink to={`/details/${element.id}`} exact>
-        <BsArrowRightCircle color="white" className={styles['nav-arrow']} />
+        <BsArrowRightCircle onClick={clickHanlder} color="white" className={styles['nav-arrow']} />
       </NavLink>
       <div className={styles.top}>
         <img src="http://pngimg.com/uploads/chess/chess_PNG8443.png" alt="chess-family" />
@@ -20,10 +27,6 @@ const Element = props => {
       </div>
     </div>
   );
-};
-
-Element.propTypes = {
-  element: PropTypes.shape.isRequired,
 };
 
 export default Element;
