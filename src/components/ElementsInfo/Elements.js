ff683140318b9ddd63e9984af1977b44d2f0a82b
Bishoy <bishoy.samwuel@gmail.com>
2021-11-19 06:09:20 +0200

    Get the elements from api

--- src/components/ElementsInfo/Elements.js
+++ src/components/ElementsInfo/Elements.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Element from './Element';
 import styles from './Elements.module.scss';
 import { getElements } from '../selectors';
+import { loadElements } from '../../redux/elements';
 
 const Elements = () => {
+  const dispatch = useDispatch();
   const elements = useSelector(getElements);
+  useEffect(() => elements.length < 10 && dispatch(loadElements()), []);
   return (
     <div className={styles['elements-div']}>
       {
-        elements.map(element => <Element key={element.id} element={element} />)
+        elements.map(element => <Element key={element.anime_id} element={element} />)
       }
     </div>
   );
