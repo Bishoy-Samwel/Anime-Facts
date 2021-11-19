82ca34920943c66d10e8116f363affc6f715b9b7
Bishoy <bishoy.samwuel@gmail.com>
2021-11-19 03:46:05 +0200

    Select the daya from the store directory

--- src/components/ElementsInfo/Elements.js
+++ src/components/ElementsInfo/Elements.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Element from './Element';
 import styles from './Elements.module.scss';
+import { getElements } from '../selectors';
 
 const Elements = () => {
-  const elements = [
-    { id: '1', name: 'Italy', number: '1' },
-    { id: '2', name: 'Italy', number: '2' },
-    { id: '3', name: 'Italy', number: '3' },
-    { id: '4', name: 'Italy', number: '4' },
-    { id: '5', name: 'Italy', number: '5' },
-    { id: '6', name: 'Italy', number: '6' },
-    { id: '7', name: 'Italy', number: '7' },
-    { id: '8', name: 'Italy', number: '8' },
-  ];
+  const elements = useSelector(getElements);
   return (
     <div className={styles['elements-div']}>
       {
