d4095d5b90633a701fbe713110365aea4adf9b1b
Bishoy <bishoy.samwuel@gmail.com>
2021-11-19 14:54:26 +0200

    Fix a selector

--- src/components/selectors.js
+++ src/components/selectors.js
@@ -2,5 +2,5 @@ import { createSelector } from 'reselect';
 
 export const getElements = state => state.elements.data;
 export const getElement = id => createSelector(
-  [getElements], elements => elements.filter(element => element.id === id),
+  [getElements], elements => elements.filter(element => element.anime_id === id),
 );
