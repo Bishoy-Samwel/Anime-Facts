ff701231f0d0155e621e9b57d136fe19b507099d
Bishoy <bishoy.samwuel@gmail.com>
2021-11-19 03:53:36 +0200

    Change content of the details

--- src/components/ElementDetails/Detail.js
+++ src/components/ElementDetails/Detail.js
@@ -1,33 +1,30 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
-import PropTypes from 'prop-types';
 import styles from './Detail.module.scss';
 
 const Detail = props => {
-  const { detail } = props;
+  const { detailKey, detail } = props;
   const { itemId } = useParams();
-
   return (
-    <div className={styles['detail-div']} key={detail.id}>
+    <div className={styles['detail-div']} key={detail}>
       <div className={styles['right-div']}>
-        {detail.name}
+        {detailKey}
+        {' '}
         +
-        {itemId}
+        {' '}
+        {detail}
 
       </div>
       <div className={styles['left-div']}>
         <span>
-          {detail.number}
+          {itemId}
         </span>
         <BsArrowRightCircle />
       </div>
     </div>
   );
-};
-
-Detail.propTypes = {
-  detail: PropTypes.shape.isRequired,
 };
 
 export default Detail;
