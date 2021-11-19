baa4ab4ebea263a4b3d94938f54996ecaeb627ae
Bishoy <bishoy.samwuel@gmail.com>
2021-11-19 03:52:47 +0200

    Modify Banner when you press return back arrow

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
 import { MdArrowBackIos, MdSettings, MdOutlineMic } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { modifyBanner } from '../redux/elements';
 import styles from './Header.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.left}>
-      <NavLink to="/">
-        <MdArrowBackIos color="white" size="20" />
-      </NavLink>
-    </div>
-    <p> page name </p>
-    <div className={styles.right}>
-      <MdOutlineMic color="white" size="20" />
-      <MdSettings color="white" size="20" />
-    </div>
-  </header>
-);
+const Header = () => {
+  const dispactch = useDispatch();
+  const clickHanlder = () => {
+    dispactch(modifyBanner());
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <NavLink to="/">
+          <MdArrowBackIos onClick={clickHanlder} color="white" size="20" />
+        </NavLink>
+      </div>
+      <p> page name </p>
+      <div className={styles.right}>
+        <MdOutlineMic color="white" size="20" />
+        <MdSettings color="white" size="20" />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
