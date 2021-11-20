9c07f5dd70f1fdb43ba6b7faef70f4d9d58db241
Bishoy <bishoy.samwuel@gmail.com>
2021-11-20 10:10:21 +0200

    use UseParams to access itemID and display the facts of this item

--- src/components/ElementDetails/Details.js
+++ src/components/ElementDetails/Details.js
@@ -1,19 +1,21 @@
-import React from 'react';
+/* eslint-disable react/jsx-key */
+/* eslint-disable max-len */
+/* eslint-disable indent */
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getElement, detailsIsLoading } from '../selectors';
 import Detail from './Detail';
-import { getElement } from '../selectors';
 
 export const Details = () => {
-  const { details } = useSelector(getElement('1'))[0];
-  return (
-    <div>
-      {
-        // eslint-disable-next-line max-len
-        Object.keys(details).map(detailKey => <Detail key={detailKey} detailKey={detailKey} detail={details[detailKey]} />)
-      }
-    </div>
-  );
+  const itemId = Number(useParams().itemId);
+  const element = useSelector(getElement(itemId));
+  const detailsLoading = useSelector(detailsIsLoading);
+
+  if (!detailsLoading && element.elemData) {
+    return (element.elemData.map(detail => <Detail detail={detail.fact} />));
+  }
+  return ('wait a minute');
 };
 
 Element.propTypes = {
