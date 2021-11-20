85dbf65ab57f3e29f18c5a16daf04cfddd73d96e
Bishoy <bishoy.samwuel@gmail.com>
2021-11-20 10:09:39 +0200

    Clean some code

--- src/components/ElementDetails/Detail.js
+++ src/components/ElementDetails/Detail.js
@@ -1,26 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
 import styles from './Detail.module.scss';
 
 const Detail = props => {
-  const { detailKey, detail } = props;
-  const { itemId } = useParams();
+  const { detail } = props;
   return (
-    <div className={styles['detail-div']} key={detail}>
+    <div className={styles['detail-div']}>
       <div className={styles['right-div']}>
-        {detailKey}
-        {' '}
-        +
-        {' '}
         {detail}
 
       </div>
       <div className={styles['left-div']}>
-        <span>
-          {itemId}
-        </span>
         <BsArrowRightCircle />
       </div>
     </div>
