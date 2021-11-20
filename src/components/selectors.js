20675f36e1394912d6b888c253e790ab01e55339
Bishoy <bishoy.samwuel@gmail.com>
2021-11-20 17:49:22 +0200

    Add a selector to get the filter term

--- src/components/selectors.js
+++ src/components/selectors.js
@@ -2,7 +2,9 @@ import { createSelector } from 'reselect';
 
 export const elementsIsLoading = state => state.elements.elementsIsLoading;
 export const detailsIsLoading = state => state.elements.detailsIsLoading;
+export const getFilterTerm = state => state.elements.filter;
 export const getElements = state => state.elements.data.slice(0, 12);
+
 export const getElement = id => createSelector(
   [getElements], elements => elements.filter(
     element => element.anime_id === id,
